414b81ce713c629778f634f500ba86620b5abbdc
Architect <31472863+Architect1st@users.noreply.github.com>
2020-07-25 12:22:15 +0800

    Create Xcode Project

---
+++ Bilibili/Shared/BilibiliApp.swift
@@ -0,0 +1,17 @@
+//
+//  BilibiliApp.swift
+//  Shared
+//
+//  Created by Dylan X on 7/25/20.
+//
+
+import SwiftUI
+
+@main
+struct BilibiliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
